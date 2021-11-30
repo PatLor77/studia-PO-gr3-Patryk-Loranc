758f576a83437f2e3d38b08a6813cd7041e82436
PatLor77 <73655971+PatLor77@users.noreply.github.com>
2021-11-30 13:12:07 +0100

    Add files via upload

---
+++ lab7/src/com/company/NazwanyPunkt.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class NazwanyPunkt extends Punkt {
+
+    private String name;
+
+    public NazwanyPunkt(int x, int y, String name){
+        super(x, y);
+        this.name = name;
+
+
+    }
+    public void show(){
+        System.out.println(name + ";<" + getX()+", " + getY() + ">");
+
+    }
+
+
+
+}

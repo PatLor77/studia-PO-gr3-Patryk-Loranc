ec47c881e2f5cd849da576f30a89106a9439053e
PatLor77 <73655971+PatLor77@users.noreply.github.com>
2021-12-07 17:16:28 +0100

    Add files via upload

---
+++ labik8/src/com/company/Fortepian.java
@@ -0,0 +1,14 @@
+package com.company.Instruments;
+
+import java.time.LocalDate;
+
+public class Fortepian extends Instrument {
+    public Fortepian(String producent, LocalDate rokProdukcji) {
+        super(producent, rokProdukcji);
+    }
+
+    @Override
+    public void dzwiek() {
+        System.out.println("TumDumDum");
+    }
+}
